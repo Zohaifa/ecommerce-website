aa4cbc1de5b0b47e49af7b741e43c07d4ccca6dc
Zohaifa <ultimatezrage@gmail.com>
2024-12-20 14:11:11 +0600

    halfway done

--- ecomm/app/models.py
+++ ecomm/app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.forms import User
 
 # Create your models here.
+CATEGORY_CHOICES=(
+('QR', 'Quran'),
+('SH', 'Sihah Sittah'),
+('HH', 'Hadith Books'),
+('DU', 'Dua Compilation'),
+)
+STATE_CHOICES=(
+    ('Chittagong', 'Chittagong'),
+    ('Dhaka', 'Dhaka'),
+    ('Sylhet', 'Sylhet'),
+    ('Barisal', 'Barisal'),
+    ('Khulna', 'Khulna'),
+    ('Rajshahi', 'Rajshahi'),
+    ('Rangpur', 'Rangpur')
+)
+class Product(models.Model):
+    title= models.CharField(max_length=100)
+    selling_price = models.FloatField() 
+    discounted_price = models.FloatField() 
+    description= models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category =models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+    
+    def _str_(self):
+        return self.title
+    
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=108)
+    
+    def _str_(self):
+        return self.name
--- ecomm/app/urls.py
+++ ecomm/app/urls.py
@@ -1,6 +1,25 @@
 from django.urls import path
 from . import views
+from . forms import LoginForm, MyPasswordResetForm, MyPasswordChangeForm
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
 
-urlpatterns = {
-    path("", views.home),
-}
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("category/<slug:val>", views.CategoryView.as_view(), name="category" ),
+    path("category-title/<val>", views.CategoryTitle.as_view(), name="category-title"),
+    path("product-detail/<int:pk>", views.ProductDetail.as_view(), name="product-detail" ),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('address/', views.address, name='address'),
+    path("update-address/<int:pk>", views.updateAddress.as_view(), name="update-address" ),
+
+    #login authentication
+    path("registration/", views.CustomerRegistrationView.as_view(), name="customerregistration" ),
+    path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('password-reset/', auth_view.PasswordResetView.as_view(template_name='app/password_reset.html', form_class=MyPasswordResetForm), name='password_reset'),
+    path('passwordchange/', auth_view.PasswordChangeView.as_view(template_name='app/changepassword.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone'), name='passwordchange'),
+    path('passwordchangedone/', auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='passwordchangedone'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
